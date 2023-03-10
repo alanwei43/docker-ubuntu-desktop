bae54328c9b0054c3f8cbc866df6bc3fa49a61e2
alan <alan.wei@live.com>
2023-03-10 22:07:35 +0800

    doc: 增加注释

--- docker-cli/push-remote.sh
+++ docker-cli/push-remote.sh
@@ -1,37 +1,64 @@
 # 本地批量推送到远程仓库
 
 # Ubuntu Desktop
+echo "push to alanway/ubuntu-desktop:18.04" >> build.log
 docker push alanway/ubuntu-desktop:18.04 && \
+echo "push to alanway/ubuntu-desktop:20.04" >> build.log
 docker push alanway/ubuntu-desktop:20.04 && \
+echo "push to alanway/ubuntu-desktop:22.04" >> build.log
 docker push alanway/ubuntu-desktop:22.04 && \
 
 # ubuntu 20.04 chrome
+echo "push to alanway/ubuntu-desktop:20.04-chrome-69" >> build.log
 docker push alanway/ubuntu-desktop:20.04-chrome-69 && \
+echo "push to alanway/ubuntu-desktop:20.04-chrome-70" >> build.log
 docker push alanway/ubuntu-desktop:20.04-chrome-70 && \
+echo "push to alanway/ubuntu-desktop:20.04-chrome-79" >> build.log
 docker push alanway/ubuntu-desktop:20.04-chrome-79 && \
+echo "push to alanway/ubuntu-desktop:20.04-chrome-80" >> build.log
 docker push alanway/ubuntu-desktop:20.04-chrome-80 && \
+echo "push to alanway/ubuntu-desktop:20.04-chrome-83" >> build.log
 docker push alanway/ubuntu-desktop:20.04-chrome-83 && \
+echo "push to alanway/ubuntu-desktop:20.04-chrome-86" >> build.log
 docker push alanway/ubuntu-desktop:20.04-chrome-86 && \
+echo "push to alanway/ubuntu-desktop:20.04-chrome-90" >> build.log
 docker push alanway/ubuntu-desktop:20.04-chrome-90 && \
+echo "push to alanway/ubuntu-desktop:20.04-chrome-102" >> build.log
 docker push alanway/ubuntu-desktop:20.04-chrome-102 && \
+echo "push to alanway/ubuntu-desktop:20.04-chrome-104" >> build.log
 docker push alanway/ubuntu-desktop:20.04-chrome-104 && \
 
+echo "push to registry.cn-hangzhou.aliyuncs.com/alanwei/ubuntu-desktop:20.04-chrome-69" >> build.log
 docker push registry.cn-hangzhou.aliyuncs.com/alanwei/ubuntu-desktop:20.04-chrome-69 && \
+echo "push to registry.cn-hangzhou.aliyuncs.com/alanwei/ubuntu-desktop:20.04-chrome-70" >> build.log
 docker push registry.cn-hangzhou.aliyuncs.com/alanwei/ubuntu-desktop:20.04-chrome-70 && \
+echo "push to registry.cn-hangzhou.aliyuncs.com/alanwei/ubuntu-desktop:20.04-chrome-79" >> build.log
 docker push registry.cn-hangzhou.aliyuncs.com/alanwei/ubuntu-desktop:20.04-chrome-79 && \
+echo "push to registry.cn-hangzhou.aliyuncs.com/alanwei/ubuntu-desktop:20.04-chrome-80" >> build.log
 docker push registry.cn-hangzhou.aliyuncs.com/alanwei/ubuntu-desktop:20.04-chrome-80 && \
+echo "push to registry.cn-hangzhou.aliyuncs.com/alanwei/ubuntu-desktop:20.04-chrome-83" >> build.log
 docker push registry.cn-hangzhou.aliyuncs.com/alanwei/ubuntu-desktop:20.04-chrome-83 && \
+echo "push to registry.cn-hangzhou.aliyuncs.com/alanwei/ubuntu-desktop:20.04-chrome-86" >> build.log
 docker push registry.cn-hangzhou.aliyuncs.com/alanwei/ubuntu-desktop:20.04-chrome-86 && \
+echo "push to registry.cn-hangzhou.aliyuncs.com/alanwei/ubuntu-desktop:20.04-chrome-90" >> build.log
 docker push registry.cn-hangzhou.aliyuncs.com/alanwei/ubuntu-desktop:20.04-chrome-90 && \
+echo "push to registry.cn-hangzhou.aliyuncs.com/alanwei/ubuntu-desktop:20.04-chrome-102" >> build.log
 docker push registry.cn-hangzhou.aliyuncs.com/alanwei/ubuntu-desktop:20.04-chrome-102 && \
+echo "push to registry.cn-hangzhou.aliyuncs.com/alanwei/ubuntu-desktop:20.04-chrome-104" >> build.log
 docker push registry.cn-hangzhou.aliyuncs.com/alanwei/ubuntu-desktop:20.04-chrome-104 && \
 
 
 # ubuntu 22.04 chrome
+echo "push to alanway/ubuntu-desktop:22.04-chrome-90" >> build.log
 docker push alanway/ubuntu-desktop:22.04-chrome-90 && \
+echo "push to alanway/ubuntu-desktop:22.04-chrome-102" >> build.log
 docker push alanway/ubuntu-desktop:22.04-chrome-102 && \
+echo "push to alanway/ubuntu-desktop:22.04-chrome-104" >> build.log
 docker push alanway/ubuntu-desktop:22.04-chrome-104 && \
 
+echo "push to registry.cn-hangzhou.aliyuncs.com/alanwei/ubuntu-desktop:22.04-chrome-90"" >> build.log
 docker push "registry.cn-hangzhou.aliyuncs.com/alanwei/ubuntu-desktop:22.04-chrome-90" && \
+echo "push to registry.cn-hangzhou.aliyuncs.com/alanwei/ubuntu-desktop:22.04-chrome-102"" >> build.log
 docker push "registry.cn-hangzhou.aliyuncs.com/alanwei/ubuntu-desktop:22.04-chrome-102" && \
+echo "push to registry.cn-hangzhou.aliyuncs.com/alanwei/ubuntu-desktop:22.04-chrome-104"" >> build.log
 docker push "registry.cn-hangzhou.aliyuncs.com/alanwei/ubuntu-desktop:22.04-chrome-104"
